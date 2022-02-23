faae9e83f631d0c89f5f07a7c6a9184358f8d193
AbdassalamAhmad <83673888+AbdassalamAhmad@users.noreply.github.com>
2022-02-23 16:35:35 +0200

    Update multiplefaces.py

--- multiplefaces.py
+++ multiplefaces.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 #from tensorflow import keras
-import cv2
-from mtcnn import MTCNN
+#import cv2
+#from mtcnn import MTCNN
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import streamlit as st
 from matplotlib import pyplot
